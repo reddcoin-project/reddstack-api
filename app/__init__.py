637c5beac49d43fe4ac9be35d8e31ae3a1fec2f4
John Nash <redd@redd.ink>
2018-10-03 09:48:19 +1000

    add eventlet support and engineio debug remove main debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+# activate eventlet
+import eventlet
+eventlet.monkey_patch(socket=True)
+
 from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.secret_key = 'development key'
-socketio = SocketIO(app)
+socketio = SocketIO(async_mode='eventlet', engineio_logger=True)
+socketio.init_app(app)
 
 from app import views
