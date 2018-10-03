d05f39fb3421b84fe43a70ad425e7a26c34ee6e8
John Nash <redd@redd.ink>
2018-10-03 12:19:42 +1000

    debug = false

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.secret_key = 'development key'
-socketio = SocketIO(async_mode='eventlet', engineio_logger=True)
+socketio = SocketIO(async_mode='eventlet', engineio_logger=False)
 socketio.init_app(app)
 
 from app import views
