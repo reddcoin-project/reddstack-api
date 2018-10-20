f9ad6a40c36e61217927e3f2fb65216bca0720c1
John Nash <redd@redd.ink>
2018-10-20 22:41:06 +1100

    display version on startup

--- reddstackapi/reddstackapid.py
+++ reddstackapi/reddstackapid.py
@@ -1,10 +1,10 @@
 #!venv/bin/python
-from reddstackapi import app, socketio, views
+from reddstackapi import app, socketio, views, config
 
 def run_reddstackapid():
 	"""
 	Run the Reddstack API server
 	"""
-	print "\nStarting API Server\n"
+	print ("\nStarting API Server v{0}\n".format(config.VERSION))
 	socketio.run(app, host='0.0.0.0', port=5000, debug=False, max_size=4096)
 
